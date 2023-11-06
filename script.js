14630b9268dae29d576bc83e76a22e4b0cf9cb5c
Nick Weber <70984558+nweber28@users.noreply.github.com>
2023-11-06 15:00:50 -0500

    Remove second scroll bar for phone view

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 function checkScreenSize() {
   var element = document.getElementById("screenCont");
+  var elemID = document.getElementById("sectionContainer");
   var screenWidth = window.innerWidth;
   var screenHeight = window.innerHeight;
 
   if (screenWidth < screenHeight) {
+    // phone
     if (element.classList.contains("row")) {
       element.classList.remove("row");
     }
     element.classList.add("column");
+    elemID.style.overflowY = "hidden"; //hide scroll bar
   } else {
     if (element.classList.contains("column")) {
       element.classList.remove("column");
     }
     element.classList.add("row");
+    elemID.style.overflowY = "auto";
   }
 }
 
